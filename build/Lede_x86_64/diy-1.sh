8941ea2eb94f061c023240e93c38155c1c44282c
hux1974 <hux1974@163.com>
2020-10-22 19:19:36 +0800

    Update diy-1.sh

--- build/Lede_x86_64/diy-1.sh
+++ build/Lede_x86_64/diy-1.sh
@@ -6,6 +6,6 @@
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
-git clone https://github.com/kenzok8/openwrt-packages.git包/ openwrt-packages
-git clone https://github.com/kenzok8/small.git包/ openwrt-small
-git clone https://github.com/tty228/luci-app-serverchan包/ luci-app-serverchan   ＃微信推
+git clone https://github.com/kenzok8/openwrt-packages.git  package/openwrt-packages
+git clone https://github.com/kenzok8/small.git  package/openwrt-small
+git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan  #微信推送
