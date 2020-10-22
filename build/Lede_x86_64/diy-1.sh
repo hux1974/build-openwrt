f06675c7747f4a287f8cc7d05d4b8252390d655d
hux1974 <hux1974@163.com>
2020-10-22 19:26:51 +0800

    Update diy-1.sh

--- build/Lede_x86_64/diy-1.sh
+++ build/Lede_x86_64/diy-1.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+#
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
 
-# 使用源码自带ShadowSocksR Plus+出国软件
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-#添加自定义插件链接（自己想要什么就github里面搜索然后添加）
+# Add a feed source
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 git clone https://github.com/kenzok8/openwrt-packages.git  package/openwrt-packages
 git clone https://github.com/kenzok8/small.git  package/openwrt-small
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan  #微信推送
